a3ed96a932cebebf3b943280d07204df3ffcbe35
jonan18 <jonan1821@gmail.com>
2022-01-24 12:32:05 +0100

    WIP: Sistema de logs

--- flask_app/order/sniffer.py
+++ flask_app/order/sniffer.py
@@ -5,8 +5,10 @@ import sys
 
 def ethernet_head(raw_data):
     dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
-    dest_mac = get_mac_addr(dest)
-    src_mac = get_mac_addr(src)
+    #dest_mac = get_mac_addr(dest)
+    #src_mac = get_mac_addr(src)
+    dest_mac = dest
+    src_mac = src
     proto = socket.htons(prototype)
     data = raw_data[14:]
     return dest_mac, src_mac, proto, data
@@ -39,20 +41,20 @@ def tcp_head(raw_data):
 def get_ip(addr):
     return '.'.join(map(str, addr))
 
+
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 while True:
     raw_data, addr = s.recvfrom(65535)
     # eth = ethernet(raw_data)
     eth = ethernet_head(raw_data)
     print('\nEthernet Frame:')
-    print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1],
-                                                             eth[2]))
-    if eth[2] == 8:
-        ipv4 = ipv4(ethp[4])
-        print('\t - ' + 'IPv4 Packet:')
-        print('\t\t - ' + 'Version: {}, Header Length: {}, TTL:{}, '.format(ipv4[1], ipv4[2], ipv4[3]))
-        print('\t\t - ' + 'Protocol: {}, Source: {}, Target:{}'.format(ipv4[4], ipv4[5], ipv4[6]))
-
+    print('Destination: {}, Source: {}, Protocol: {}'.format(eth[0], eth[1], eth[2]))
+#    if eth[2] == 8:
+#        ipv4 = ipv4(ethp[4])
+#        print('\t - ' + 'IPv4 Packet:')
+#        print('\t\t - ' + 'Version: {}, Header Length: {}, TTL:{}, '.format(ipv4[1], ipv4[2], ipv4[3]))
+#        print('\t\t - ' + 'Protocol: {}, Source: {}, Target:{}'.format(ipv4[4], ipv4[5], ipv4[6]))
+    """
         if ipv4[4] == 6:
             tcp = tcp_head(ipv4[7])
             print(TAB_1 + 'TCP Segment:')
@@ -87,3 +89,4 @@ while True:
             udp = udp_head(ipv4[7])
             print('\t -' + 'UDP Segment:')
             print('\t\t -' + 'Source Port: {}, Destination Port: {}, Length:{}'.format(udp[0], udp[1], udp[2]))
+        """
