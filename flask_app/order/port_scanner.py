894c1e6d07a81a7687b20fa869114d2c3e978c2f
jonan18 <jonan1821@gmail.com>
2022-01-24 11:33:10 +0100

    WIP: Sistema de logs

--- flask_app/order/port_scanner.py
+++ flask_app/order/port_scanner.py
@@ -1,8 +1,8 @@
 import socket
 
 
-def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-    while True:
-        print(s.recvfrom(65565))
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+while True:
+    print(s.recvfrom(65565))
+
 
